62324e74103d27bf7729f252289efb1d41b7ccc4
jaydip <jadavjaydip6000@gmail.com>
2024-11-05 09:43:54 +0530

    api services

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,12 @@
-import 'dart:convert';
-
+import 'package:get/get.dart';
 import 'package:single_api/models/post.dart';
-import 'package:http/http.dart' as http;
 
-class ApiService {
+class ApiService extends GetConnect {
   Future<Post> fetchPost() async {
-    final response = await http
-        .get(Uri.parse("https://jsonplaceholder.typicode.com/posts/1"));
+    final response = await get("https://jsonplaceholder.typicode.com/posts/1");
 
     if (response.statusCode == 200) {
-      Post data = Post.fromJson(jsonDecode(response.body));
+      Post data = Post.fromJson(response.body);
       return data;
     } else {
       throw Exception('Failed to load post');
